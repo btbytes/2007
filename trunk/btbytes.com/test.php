06e77bdc3cacf92557caaeb3b79e4b1cbed42087
pradeep.gowda <pradeep.gowda@f209512b-9e19-0410-93b8-95f87e0ec697>
2007-11-27 02:19:07 +0000

    chagnes

--- trunk/btbytes.com/test.php
+++ trunk/btbytes.com/test.php
@@ -1,16 +1,53 @@
-<?php get_header() ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" dir="ltr">
+<head profile="http://gmpg.org/xfn/11">
+	<title> Test document </title>
+	<meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+	<meta name="description" content="Ploughing the bit fields one byte at a time." />	
+	<link rel="stylesheet" type="text/css" href="http://yui.yahooapis.com/2.3.1/build/grids/grids-min.css"> 
+	<link rel="stylesheet" type="text/css" href="http://www.btbytes.com/wordpress/wp-content/themes/kshira/style.css" />
+	<link rel="alternate" type="application/rss+xml" href="http://feeds.feedburner.com/btbytes_rss2" title="BTBYTES.com Posts RSS feed" />
+
+	<link rel="alternate" type="application/rss+xml" href="http://www.btbytes.com/comments/feed/" title="BTBYTES.com Comments RSS feed" />
+
+<!-- GBCF -->
+<link rel="stylesheet" type="text/css" media="screen" href="http://www.btbytes.com/wordpress/wp-content/plugins/wp-gbcf/wp-gbcf_themes/default.css">
+<!--[if IE]><script src="http://www.btbytes.com/wordpress/wp-content/plugins/wp-gbcf/wp-gbcf_focus.js" type="text/javascript"></script><![endif]-->
+<!-- GBCF -->
+
+<script type="text/javascript" charset="utf-8">
+	function fnSearch(e){
+		var searchform = document.getElementById("searchform");
+		searchform.focus();
+	}
+</script>
+</head>
+
+<body>
+
+<div id="doc" class="hfeed">
+<div id="hd">
+<img src="/btbytes.png" id="logo">
+<div id="glob-nav">
+<a href="/"><img src="/icon/house.png"></a>&nbsp;
+<a href="/about/"><img src="/icon/user.png"></a>&nbsp;
+<a href="/contact/"><img src="/icon/email.png"></a>&nbsp;
+<a href="/feed/"><img src="/icon/feed.png"></a>&nbsp;
+<a href="#search" onclick="fnSearch()" ><img src="/icon/magnifier.png" /></a>&nbsp;
+</div>
+</div><!--  #header -->
+<!-- PKG -->
+<div id="header-news">
+<iframe src="http://www.adpinion.com/app/adpinion_frame?website=118227&width=468&height=60" id="adframe" allowtransparency="true" style="width:486px;height:77px;" scrolling="no" frameborder="0"></iframe>
+</div>
+<div id="access">
+<div class="skip-link"><a href="#content" title="Skip navigation to the content">Skip to content</a></div>
+<div id="menu"><ul><li class="page_item page-item-462"><a href="http://www.btbytes.com/myhome/" title="New Home">New Home</a></li><li class="page_item page-item-465"><a href="http://www.btbytes.com/archives/" title="Archives">Archives</a></li><li class="page_item page-item-417"><a href="http://www.btbytes.com/contact/" title="Contact">Contact</a></li><li class="page_item page-item-413"><a href="http://www.btbytes.com/about/" title="About">About</a></li></ul></div>
+</div><!-- #access -->
 
 <div id="bd">
 <div id="content">
-  <div id="nav">
-    <ul>
-      <li><a href="index2.html">Home</a></li>
-<li>&gt;</li>
-<li>Text</li>
-
-    </ul>
-  </div>
-  <div class="document" id="test-document">
+ <div class="document" id="test-document">
 <h1 class="title">Test Document</h1>
 <p>Trying to write out PHP files using rest2web.</p>
 </div>
@@ -18,4 +55,27 @@
 
 </div><!-- #content -->
 </div><!-- #container -->
-<?php get_footer() ?>
+
+<div id="footer">
+<a name="search"></a>
+		<form id="searchform" method="get" action="http://www.btbytes.com">
+
+			<div>
+				<input id="s" name="s" type="text" value="" size="10" tabindex="1" />
+				<input id="searchsubmit" name="searchsubmit" type="submit" value="Find" tabindex="2" />
+			</div>
+</form>
+<a href="/archives/">Archives</a> | <a href="http://wiki.btbytes.com/">Wiki</a> | <a href="/about/">About</a> | <a href="/contact/">Contact</a> | <br/>
+
+<span>&copy; MMVII PKG</span>
+</div><!-- #footer -->
+</div><!-- #wrapper .hfeed -->
+
+<script src="http://www.google-analytics.com/urchin.js" type="text/javascript">
+</script>
+<script type="text/javascript">
+_uacct = "UA-103521-1";
+urchinTracker();
+</script>
+</body>
+</html>
