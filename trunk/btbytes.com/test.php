82f31815251031315a4907efdbc54168449a2891
pradeep.gowda <pradeep.gowda@f209512b-9e19-0410-93b8-95f87e0ec697>
2007-11-27 01:48:41 +0000

    adding test

---
+++ trunk/btbytes.com/test.php
@@ -0,0 +1,21 @@
+<?php get_header() ?>
+
+<div id="bd">
+<div id="content">
+  <div id="nav">
+    <ul>
+      <li><a href="index2.html">Home</a></li>
+<li>&gt;</li>
+<li>Text</li>
+
+    </ul>
+  </div>
+  <div class="document" id="test-document">
+<h1 class="title">Test Document</h1>
+<p>Trying to write out PHP files using rest2web.</p>
+</div>
+
+
+</div><!-- #content -->
+</div><!-- #container -->
+<?php get_footer() ?>
